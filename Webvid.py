fd298e22d601b45807455719d8e2937469ae72cb
lvdev <lov3r.dev@gmail.com>
2024-10-08 22:25:59 +0100

    testing functionality

--- Webvid.py
+++ Webvid.py
@@ -22,3 +22,23 @@ def download_video_series(video_links):
         #obtain filename by splitting url and getting last string
         file_name = link.split('/')[-1]
         print("Downloading file:%s"%file_name)
+
+        #create response object
+        r = requests.get(link, stream = True)
+        
+        #download started
+        with open(file_name, 'wb') as f:
+            for chunk in r.inter_content(chunk_size = 1024*1024):
+                if chunk:
+                    f.write(chunk)
+
+        print("%s downloaded!\n"%file_name)
+
+    print("All videos downloaded!")
+    return
+
+if __name__ == '__main__':
+    #getting all video links
+    video_links = get_video_links()
+    #download all videos
+    download_video_series(video_links)
