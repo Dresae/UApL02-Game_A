4f449d90d18c7bd0a452ee6ab5fab5065cc47164
lvdev <lov3r.dev@gmail.com>
2024-10-08 22:10:58 +0100

    defining function that download the video files

--- Webvid.py
+++ Webvid.py
@@ -7,4 +7,18 @@ def get_video_links():
     #create response objects
     r = requests.get(archive_url)
     #create BeautifulSoup object
-    soup = BeautifulSoup(r.content, 'html5')
+    soup = BeautifulSoup(r.content, 'html5lib')
+    #find all video links on web page
+    links = soup.findAll('a')
+    #filter the link ending in .mp4
+    video_links = [archive_url + link['href'] for link in links if link['href'].endswith('mp4')]
+
+    return video_links
+
+def download_video_series(video_links):
+    for link in video_links:
+
+        #iterate through all links in video_links & dowload them one by one
+        #obtain filename by splitting url and getting last string
+        file_name = link.split('/')[-1]
+        print("Downloading file:%s"%file_name)
